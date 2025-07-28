9bf75e9b0caf48f0fb1c9352476fb5a877aaf95e
Faisal <faisalsagheerqureshi@gmail.com>
2025-07-28 12:20:22 +0500

    first commit

---
+++ app/(pages)/home/page.js
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      Home Page
+    </div>
+  )
+}
+
+export default Home
---
+++ app/components/register-form.jsx
@@ -0,0 +1,58 @@
+
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/app/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/app/components/ui/card"
+import { Input } from "@/app/components/ui/input"
+import { Label } from "@/app/components/ui/label"
+
+export function RegisterForm({
+  className,
+  ...props
+}) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Register your account</CardTitle>
+          <CardDescription>
+            Enter your email below to regidter your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="m@example.com" required />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Register
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}

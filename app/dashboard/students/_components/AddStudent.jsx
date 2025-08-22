f8f217d326729a08a5c78695c12183b2f0b9bf46
Faisal <faisalsagheerqureshi@gmail.com>
2025-08-22 11:17:50 +0500

    chgs

--- app/dashboard/students/_components/AddStudent.jsx
+++ app/dashboard/students/_components/AddStudent.jsx
@@ -14,49 +14,50 @@ import { Input } from "@/app/components/ui/input";
 import { Label } from "@/app/components/ui/label";
 import { Plus } from "lucide-react";
 import { useForm } from "react-hook-form";
+import { toast, ToastContainer } from "react-toastify";
 
 function AddStudent() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log("FormData",data);
- 
+  const onSubmit = data => console.log("FormData", data);
+
   return (
     <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="outline">
-            <Plus />
-            Add Student
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Add New Student</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you&apos;re
-              done.
-            </DialogDescription>
-          </DialogHeader>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <Plus />
+          Add Student
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add New Student</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you&apos;re
+            done.
+          </DialogDescription>
+        </DialogHeader>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="grid gap-4">
             <div className="grid gap-3">
               <Label htmlFor="name-1">FullName :</Label>
-              <Input  {...register("name")}/>
+              <Input  {...register("name", { required: true })} />
             </div>
             <div className="grid gap-3">
               <Label htmlFor="address">Address :</Label>
               <Input
-                
-                {...register("address")}
+
+                {...register("address", { required: true })}
               />
             </div>
             <div className="grid gap-3">
               <Label >Contact :</Label>
-              <Input  
-              {...register("contact")}/>
+              <Input
+                {...register("contact", { required: true })} />
             </div>
             <div className="grid gap-3">
               <Label htmlFor="select">Select Grade : </Label>
               <select className="p-3 border rounded-lg"
-              {...register('grade',{required:true})}>
+                {...register('grade', { required: true })}>
                 <option value={"5th"}>5th</option>
                 <option value={"6th"}>6th</option>
                 <option value={"7th"}>7th</option>
@@ -69,8 +70,10 @@ function AddStudent() {
             </DialogClose>
             <Button type="submit">Save</Button>
           </DialogFooter>
-      </form>
-        </DialogContent>
+          <Input {...register('example',{required:true})}/>
+          {errors.example && <span>This field is required</span>}
+        </form>
+      </DialogContent>
     </Dialog>
   );
 }
